e44444d8d06c4c0977b532304dad4d9cde3a403a
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-04-29 12:02:10 +0100

    step028

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ServerStatus from './ServerStatus'
+
 
 Vue.config.productionTip = false
+
+Vue.component('server-status', ServerStatus);
 
 new Vue({
   render: h => h(App),
