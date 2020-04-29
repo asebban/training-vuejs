fe434af77d8100f7a37dcc5ddf9e68570ed1ea1e
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-04-29 12:12:28 +0100

    ex006

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,41 @@
 <template>
-    <servers></servers>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <header>
+          <h1>Server Status</h1>
+        </header>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+          <li
+            class="list-group-item"
+            v-for="index in 5" v-bind:key="index">
+            Server #{{ index }}
+          </li>
+        </ul>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <p>Server Details are currently not updated</p>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12">
+        <footer>
+          <p>All Servers are managed here</p>
+        </footer>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default { }
 </script>
 
 <style>
+
 </style>
