e75c2b77e63a0f3aa7ae796261b3a03d322dd2cb
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-04-30 10:20:56 +0100

    step032

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div class="container">
-    <app-header></app-header>
-    <hr>
     <div class="row">
-      <app-servers></app-servers>
-      <app-server-details></app-server-details>
+      <div class="col-xs-12">
+        <app-user></app-user>
+      </div>
     </div>
-    <hr>
-    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-  import Header from './components/commons/Header.vue';
-  import Footer from './components/commons/Footer.vue';
-  import Servers from './components/servers/Servers.vue';
-  import ServerDetails from './components/servers/ServerDetails.vue';
+  import User from './components/User.vue';
 
   export default {
     components: {
-      'app-header': Header,
-      'app-servers': Servers,
-      'app-server-details': ServerDetails,
-      'app-footer': Footer
+      appUser: User
     }
   }
 </script>
 
 <style>
-
+  div.component {
+    border: 1px solid black;
+    padding: 30px;
+  }
 </style>
