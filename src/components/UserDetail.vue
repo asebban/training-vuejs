53c938a87470b7210a58abe32a551ef23c5a54f7
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-04-30 10:29:08 +0100

    step034

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,14 +2,21 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details. My name is {{ name }}</p>
+        <button @click="name = 'Ahmed'">Reset Name</button>
     </div>
 </template>
 
 <script>
   export default {
-    props: ['name']
+    props: {
+      name: {
+        type: String,
+        required: true
+      }
+    }
   }
 </script>
+
 
 <style scoped>
     div {
