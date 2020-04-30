259ab8efb62c09da63ac04d21073f16897c27f12
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-04-30 11:28:49 +0100

    step037

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
-    <div class="container">
-        <app-header></app-header>
-        <hr>
-        <div class="row">
-            <servers></servers>
-            <app-server-details></app-server-details>
-        </div>
-        <hr>
-        <app-footer></app-footer>
-    </div>
+<div>
+  <app-quote></app-quote>
+</div>
 </template>
 
 <script>
-    import Header from './components/shared/Header.vue';
-    import Footer from './components/shared/Footer.vue';
-    import Servers from './components/server/Servers.vue';
-    import ServerDetails from './components/server/ServerDetails.vue';
 
-    export default {
-        components: {
-            appHeader: Header,
-            Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        }
+  import Quote from './components/Quote';
+
+  export default {
+    components: {
+      'app-quote': Quote
     }
+  }
 </script>
 
 <style>
