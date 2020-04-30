98583b5d0211a35c208e029e54bf4d5a39120351
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-04-30 11:55:26 +0100

    step041

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,24 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-md-4">
-      <input type="text" class="form-control" v-model.number="user.name">
-        <textarea name="description" id="description" cols="30" rows="10" v-model="user.description"></textarea>
-      </div>
-      <div class="col-md-offset-8"></div>
-    </div>
-    <div class="form-group">
-      <label for="sendmail">
-        <input type="checkbox" id="sendmail" class="form-control" value="SendMail" v-model="user.sendMail">Send Mail
-      </label>
-      <label for="sendInfoMail">
-        <input type="checkbox" value="SendInfoMail" class="form-control" id="sendInfoMail" v-model="user.sendMail">Send info mail
-      </label>
-    </div>
-    <p>{{ user.name }}</p>
-    <p style="white-space: pre">{{ user.description }}</p>
-    <p>Send Mail: {{ user.sendMail }}</p>
+    <app-switch v-model="dataSwitch"></app-switch>
+    Switched: {{ dataSwitch }}
   </div>
 </template>
 
 <script>
+  import Switch from './components/Switch';
+
   export default {
     data() {
       return {
-        user: {
-          name: 'Ahmed',
-          age: 30,
-          description: 'Some description',
-          sendMail: []
-        }
+        dataSwitch: true
       }
+    },
+    components: {
+      'app-switch' : Switch
     }
   }
 </script>
 
 <style scoped>
-  p {
-    border: solid 1px;
-  }
 </style>
