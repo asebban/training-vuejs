23bd18c0107aecdd2ea44aac2e8d6e5b72eba4fa
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 14:11:35 +0100

    step072

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,18 @@
     <div class="container">
       <app-counter></app-counter>
       <app-result></app-result>
-      <app-another-result></app-another-result>
     </div>
 </template>
 
 <script>
   import Counter from './components/Counter';
   import Result from './components/Result';
-  import AnotherResult from './components/AnotherResult';
 
   export default {
 
     components: {
       appCounter: Counter,
-      appResult: Result,
-      appAnotherResult: AnotherResult
+      appResult: Result
     }
   }
 </script>
