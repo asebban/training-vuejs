1dc990bfa9ef2f5544bf145fe994bff5b04be525
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 12:03:57 +0100

    step069

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -2,6 +2,8 @@
   <div>
     <h3>Edit the User ({{ id }})</h3>
     <p>User name : {{ $route.query.name }}</p>
+    <div style="height: 900px"></div>
+    <p id="data">This is some extra data</p>
   </div>
 </template>
 
@@ -12,6 +14,10 @@
         return {
           id: this.$route.params.id
         }
+      },
+      beforeRouteLeave(to, from, next) {
+        console.log('before Leaving Edit to '+to.path);
+        next();
       }
     }
   </script>
