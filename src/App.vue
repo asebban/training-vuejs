695d2ca1bfd18382620e725fc3d25741407e7687
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 09:45:13 +0100

    ex009

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="container">
-    <p v-highlight.delayed="'red'">Texte</p>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Exercices Directives</h1>
+        <!-- Exercice -->
+        <!-- Construire une directive personnalisée qui marche comme v-on (écoute les évènements) -->
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-  export default {
-  }
+  export default {}
 </script>
 
-<style scoped>
+<style>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,25 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.directive('highlight', {
-  bind(el, binding){
-
-    var delay = 0;
-
-    if (binding.modifiers['delayed']) {
-      delay = 3000;
-    }
-
-    setTimeout(() => {
-      if (binding.arg == 'background')
-        el.style.backgroundColor = binding.value;
-      else
-        el.style.color = binding.value;
-    },delay);
-  }
-})
-
-
 new Vue({
   el: '#app',
   render: h => h(App)
