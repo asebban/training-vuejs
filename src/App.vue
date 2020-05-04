dd431ce978af2514e0bdaa2e9fa06252d15014a6
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 10:28:31 +0100

    step050

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,12 @@
 <template>
   <div class="container">
-    <button class="btn btn-primary" @click="show = !show">Toggle alert</button>
-    <transition name="fade">
-      <div class="alert alert-info" v-if="show">My message is here!</div>
-    </transition>
   </div>
 </template>
 
 <script>
   export default {
-    data() {
-      return {
-        show: false
-      }
-    }
   }
 </script>
 
 <style>
-  .fade-enter {
-    opacity: 0;
-  }
-
-  .fade-enter-active {
-    transition: opacity 1s;
-  }
-
-  .fade-leave {
-
-  }
-
-  .fade-leave-active {
-    transition: opacity 1s;
-    opacity: 0;
-  }
 </style>
