17acd9914b1a1ba95f0579939e062b2e3ac5d539
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 11:26:26 +0100

    step059

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,17 @@
 <template>
+  <div>
     <h1>The User Page</h1>
-
+    <hr>
+    <button class="btn btn-primary" @click="navigateToHome">Navigate to Home</button>
+  </div>
 </template>
+
+<script>
+  export default {
+    methods: {
+      navigateToHome() {
+        this.$router.push('/');
+      }
+    }
+  }
+</script>
