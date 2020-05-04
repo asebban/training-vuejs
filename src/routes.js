6f4487925bbea153b8ef2b696a808ef614450f8f
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 11:50:10 +0100

    step065

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,21 @@ import UserDetail from './components/user/UserDetail'
 import UserEdit from './components/user/UserEdit'
 import UserStart from './components/user/UserStart'
 import Home from './components/Home'
+import Header from './components/Header'
 
 export const routes = [
-  { path: '/user', component: User, children: [
+  { path: '/user',
+    components: {
+      default: User,
+      'header-bottom': Header
+    },
+    children: [
       { path: '', component: UserStart},
       { path: '/:id', component: UserDetail},
       { path: '/:id/edit', component: UserEdit, name: 'userEdit'}
     ]},
-  { path: '/', component: Home }
+  { path: '/', components: {
+      default: Home,
+      'header-top': Header
+    } }
 ];
