517b2445f7768f3017e0d63a974e584821a842aa
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 10:07:58 +0100

    ex010

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.filter('toLowerCase', value => value.toLowerCase());
+
+Vue.filter('to-lowercase', function(value) {
+  return value.toLowerCase();
+});
+
+Vue.mixin({
+  created() {
+    console.log('Global Mixin - Created Hook');
+  }
+});
 
 new Vue({
   el: '#app',
