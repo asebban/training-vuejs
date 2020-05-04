0c6ef669ccb048e43a9b4bcdc5b1e22afe6faf92
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 09:52:55 +0100

    step046

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,38 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Directives Exercise</h1>
-        <!-- Exercise -->
-        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-        <button v-customOn:click="clicked" class="btn btn-primary">Click Me</button>
-        <hr>
-        <div
-          style="width: 100px; height: 100px; background-color: lightgreen"
-          v-customOn:mouseenter="mouseEnter"
-          v-customOn:mouseleave="mouseLeave"></div>
-      </div>
-    </div>
+    <p v-highlight.delayed="'red'">Texte</p>
   </div>
 </template>
 
 <script>
+
   export default {
-    directives: {
-      customOn: {
-        bind(el, binding) {
-//                    el.onclick = function() {
-//                        binding.value();
-//                    }
-          const type = binding.arg;
-          const fn = binding.value;
-          el.addEventListener(type, fn);
-        }
+    data() {
+      return {
+        dataSwitch: true
       }
     },
-    methods: {
-      clicked() {
-        alert('I was clicked!');
-      },
-      mouseEnter() {
-        console.log('Mouse entered!');
-      },
-      mouseLeave() {
-        console.log('Mouse leaved!');
+    directives: {
+      'highlight': {
+        bind(el, binding){
+
+          var delay = 0;
+
+          if (binding.modifiers['delayed']) {
+            delay = 3000;
+          }
+
+          setTimeout(() => {
+            if (binding.arg == 'background')
+              el.style.backgroundColor = binding.value;
+            else
+              el.style.color = binding.value;
+          },delay);
+        }
       }
     }
   }
 </script>
 
-<style>
+<style scoped>
 </style>
