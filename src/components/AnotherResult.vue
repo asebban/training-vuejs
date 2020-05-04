30f0febf460dbe337c6465e2ffd609040ba749ab
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 14:15:54 +0100

    step074

---
+++ src/components/AnotherResult.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  {{ myOwn }}, Counter is {{ doubleCounter }}
+</div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+    export default {
+        computed: {
+          ...mapGetters([ 'doubleCounter']),
+          myOwn() {
+            return 'Hello';
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
