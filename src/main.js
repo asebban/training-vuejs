8ce46c2915591f81db5a535bb1e9bff40b6242b3
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 10:56:41 +0100

    step054

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import VueResource from 'vue-resource';
 Vue.use(VueResource);
 
 Vue.http.options.root = 'http://localhost:3000/employees';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request.method);
+  next(response => {
+    console.log('intercepted response ',response);
+  });
+});
+
 
 new Vue({
   el: '#app',
