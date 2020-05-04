523b89ed7f3daab945e654dd918bf0ae09f20246
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 09:40:46 +0100

    step044

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 
 Vue.directive('highlight', {
   bind(el, binding){
-    el.style.backgroundColor = binding.value;
+    if (binding.arg == 'background')
+      el.style.backgroundColor = binding.value;
+    else
+      el.style.color = binding.value;
   }
 })
 
