a50e8ffc2359b45f1cf47b35523704ffed11e1d1
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 10:33:21 +0100

    step051

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
   <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" v-model="user.username">
+        </div>
+        <div class="form-group">
+          <label >Mail</label>
+          <input type="text" class="form-control" v-model="user.email">
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        user: {
+          username: '',
+          email: ''
+        }
+      }
+    },
+    methods: {
+      submit() {
+        this.$http.post('http://localhost:3000/employees', this.user);
+      }
+    }
   }
 </script>
 
