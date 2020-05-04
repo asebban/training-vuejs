7a37f61c2bc0a336e11c9ca902c3a67b498bf884
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 10:20:07 +0100

    step049

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Filters & Mixins</h1>
-        <!-- Exercice 1) -->
-        <!-- Construire un filtre local qui renverse le texte sur lequel il est appliqué -->
-
-        <!-- Exercice 2 -->
-        <!-- construire un filtre global qui calcule la longueur du mot et l'ajoute entre parenthèse au texte -->
-        <!-- Comme ceci: "Test" => Est filtré à => "Test (4)" -->
-
-        <!-- Exercice 3 -->
-        <!-- Faire la même chose que l'exercice 1 et 2, mais avec les Computed Properties -->
-
-        <!-- Exercice 4 -->
-        <!-- Partager la Computed Property et refaire ainsi l'Exercice 2 via un Mixin -->
-      </div>
-    </div>
+    <button class="btn btn-primary" @click="show = !show">Toggle alert</button>
+    <transition name="fade">
+      <div class="alert alert-info" v-if="show">My message is here!</div>
+    </transition>
   </div>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        show: false
+      }
+    }
   }
 </script>
 
 <style>
+  .fade-enter {
+    opacity: 0;
+  }
+
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+
+  .fade-leave {
+
+  }
+
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+  }
 </style>
