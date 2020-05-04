8ba0a6f9b53931726bf9450dedfb1a63772d5497
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 12:07:26 +0100

    step070

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
-import User from './components/user/User'
 import UserDetail from './components/user/UserDetail'
 import UserEdit from './components/user/UserEdit'
 import UserStart from './components/user/UserStart'
 import Home from './components/Home'
 import Header from './components/Header'
+
+const User = resolve => {
+  require.ensure(['./components/user/User'], () => {
+    resolve(require('./components/user/User'))
+  })
+};
 
 export const routes = [
   { path: '', components: {
