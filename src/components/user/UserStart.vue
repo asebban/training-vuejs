1c1081da448ac908da0badf247ae55c077b85aa2
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 11:52:58 +0100

    step066

--- src/components/user/UserStart.vue
+++ src/components/user/UserStart.vue
@@ -3,9 +3,9 @@
         <p>Please select a User</p>
         <hr>
         <ul class="list-group">
-            <router-link tag="li" to="/1" class="list-group-item" style="cursor: pointer" >User 1</router-link>
-            <router-link tag="li" to="/2" class="list-group-item" style="cursor: pointer" >User 2</router-link>
-            <router-link tag="li" to="/3" class="list-group-item" style="cursor: pointer" >User 3</router-link>
+            <router-link tag="li" to="/user/1" class="list-group-item" style="cursor: pointer" >User 1</router-link>
+            <router-link tag="li" to="/user/2" class="list-group-item" style="cursor: pointer" >User 2</router-link>
+            <router-link tag="li" to="/user/3" class="list-group-item" style="cursor: pointer" >User 3</router-link>
         </ul>
     </div>
 </template>
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,11 @@ import Home from './components/Home'
 import Header from './components/Header'
 
 export const routes = [
+  { path: '', components: {
+      default: Home,
+      'header-top': Header
+    }
+  },
   { path: '/user',
     components: {
       default: User,
@@ -13,11 +18,8 @@ export const routes = [
     },
     children: [
       { path: '', component: UserStart},
-      { path: '/:id', component: UserDetail},
-      { path: '/:id/edit', component: UserEdit, name: 'userEdit'}
+      { path: ':id', component: UserDetail},
+      { path: ':id/edit', component: UserEdit, name: 'userEdit'}
     ]},
-  { path: '/', components: {
-      default: Home,
-      'header-top': Header
-    } }
+  { path: '*', redirect: '/'}
 ];
