5874d01ecc2f4b482332a3ad532a7b28d56381f6
Amine BOUAGGAD <abouaggad@omnidata.ma>
2020-05-04 10:48:56 +0100

    step052

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,13 @@
           <input type="text" class="form-control" v-model="user.email">
         </div>
         <button class="btn btn-primary" @click="submit">Submit</button>
+        <button class="btn btn-primary" @click="fetch">Fetch</button>
       </div>
     </div>
+    <hr>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="employee in employees" v-bind:key="employee.id">{{ employee.username }}</li>
+    </ul>
   </div>
 </template>
 
@@ -24,14 +29,29 @@
         user: {
           username: '',
           email: ''
-        }
+        },
+        employees: []
       }
     },
     methods: {
       submit() {
-        this.$http.post('http://localhost:3000/employees', this.user);
-        this.user.username='';
-        this.user.email='';
+        this.$http.post('http://localhost:3000/employees', this.user)
+          .then(response => {
+            console.log(response);
+            this.user.username='';
+            this.user.email='';
+            this.fetch();
+          }, error => {
+            console.log(error);
+          });
+      },
+      fetch() {
+        this.$http.get('http://localhost:3000/employees')
+          .then(response => {
+            this.employees = response.body;
+          }, error => {
+            console.log(error);
+          });
       }
     }
   }
